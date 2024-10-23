a1ff737f66ccdeb217d46b3d754d24541325e835
StarPandaBeg <kshmurakov@mail.ru>
2024-10-23 22:36:30 +0300

    feat: proxy api requests

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 
 const nextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
