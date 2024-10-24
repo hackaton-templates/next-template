c110ae4b1181fbc301594c0029db609a6bb4ea1e
StarPandaBeg <kshmurakov@mail.ru>
2024-10-24 23:47:06 +0300

    feat: add /sign-out route

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -8,8 +8,25 @@ export interface AuthResult {
   refreshToken: string;
 }
 
+export interface User {
+  id: string;
+  lastName: string;
+  firstName: string;
+  middleName: string;
+  email: string;
+  roles: string[];
+}
+
 export async function auth(credentials: AuthCredentials) {
   return await apiFetch<AuthResult>("/api/login", credentials, {
     method: "POST",
   });
+}
+
+export async function logout() {
+  await apiFetch("/api/logout");
+}
+
+export async function me() {
+  return await apiFetch<User>("/api/profile");
 }
