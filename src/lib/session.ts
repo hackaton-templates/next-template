d6092134cf15c02642221fd71669e1c39a828662
StarPandaBeg <kshmurakov@mail.ru>
2024-10-24 23:46:19 +0300

    feat: add session functions

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -11,6 +11,21 @@ export async function createSession<T>(data: T) {
   });
 }
 
+// May be changed in future
+export async function updateSession<T>(data: T) {
+  return await createSession(data);
+}
+
+export async function getSession<T>() {
+  const data = cookies().get("session");
+  if (!data || !data.value) return undefined;
+  return decrypt(data.value) as T;
+}
+
+export async function deleteSession() {
+  cookies().delete("session");
+}
+
 async function encrypt<T>(data: T) {
   return JSON.stringify(data);
 }
